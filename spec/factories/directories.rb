eb72b7204024329f6bb46f62427f93e5d6d9c95b
Paweł Dawczak <pawel.dawczak@gmail.com>
2014-09-25 20:29:59 +0100

    Added factories

--- spec/factories/directories.rb
+++ spec/factories/directories.rb
@@ -8,4 +8,20 @@ FactoryGirl.define do
   factory :ice_dir, class: Directory do
     name 'ice'
   end
+
+  factory :assets_dir, class: Directory do
+    name 'assets'
+  end
+
+  factory :js_dir, class: Directory do
+    name 'js'
+  end
+
+  factory :css_dir, class: Directory do
+    name 'css'
+  end
+
+  factory :documents_dir, class: Directory do
+    name 'documents'
+  end
 end
